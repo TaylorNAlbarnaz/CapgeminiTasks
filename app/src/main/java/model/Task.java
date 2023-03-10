817cea2211242e6fafbeccba5eb905b8a581c22b
TaylorNAlbarnaz <veloxgods@gmail.com>
2023-03-10 15:53:44 -0300

    Finishes the Task Controller

--- app/src/main/java/model/Task.java
+++ app/src/main/java/model/Task.java
@@ -32,6 +32,11 @@ public class Task {
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
     }
+    
+    public Task() {
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+    }
 
     public int getId() {
         return id;
--- app/src/main/java/util/ConnectionFactory.java
+++ app/src/main/java/util/ConnectionFactory.java
@@ -6,6 +6,8 @@ package util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  *
@@ -30,6 +32,39 @@ public class ConnectionFactory {
         try {
             if (connection != null) {
                 connection.close();
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro ao fechar a conexão com banco de dados", ex);
+        }
+    }
+    
+    public static void closeConnection(Connection connection, PreparedStatement statement) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+            
+            if (statement != null) {
+                statement.close();
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro ao fechar a conexão com banco de dados", ex);
+        }
+    }
+    
+    public static void closeConnection(Connection connection, PreparedStatement statement, 
+            ResultSet resultSet) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+            
+            if (statement != null) {
+                statement.close();
+            }
+            
+            if (resultSet != null) {
+                resultSet.close();
             }
         } catch (Exception ex) {
             throw new RuntimeException("Erro ao fechar a conexão com banco de dados", ex);
